9253d54d71097d43c0c857b7786fae1d9771dcf5
OLECSII MIR <zachhytnyk@live.com>
2021-10-24 20:42:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace SMSCtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            
+            
+            SMSC sms = new SMSC();
+            string[] r = sms.send_sms("","Hello");
+
+        }
+
+
+
+       
+    }
+}
+
+//https://smsc.ua/api/http/
+
+//Реализовать работу с api провайдера сообщений
+
+//В виде класса
+
+//Интерфейс
+//-ISMSCMessage
+//Классы
+//-SmsMessage
+//-ViberMessage
+
+//-Credentials (авторизация)
+
+//Service
+// + Service(credentials: Credentials)
+// + getBalance(): float
+// +sendMessage(message: ISMSCMessage): bool
+// +getMessageCost(message: ISMSCMessage): float
+// +getStatus(message: ISMSCMessage): int
